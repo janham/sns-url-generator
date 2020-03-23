bd11d2ca648f8bfbe87567c66b0a52cf0e574c70
hironori <hironori.yamada@w-heads.com>
2020-03-24 01:34:10 +0900

    finished setting modal section

---
+++ src/js/effects.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+    $(".copy-btn").on({
+        'mouseenter':function(){
+            var text = $(this).attr('data-text');
+            if(text === 'copy'){
+                chipText = 'Copy'
+            }
+            $(this).append('<div class="tooltips">' + chipText + '</div>');
+        },
+        'mouseleave':function(){
+            $(this).find(".tooltips").remove();
+        }
+    });
+
+    $(window).on('scroll', function(){
+        let scrollTop = $(window).scrollTop();
+        if(scrollTop >= 160){
+            $('.header').slideUp();
+        }else{
+            $('.header').slideDown();
+
+        }
+    });
+
+    $('.header__tag-g-icon-wrap').on('click', function() {
+        $('#modal').fadeIn();
+        $('.modal__mask').fadeIn();
+    });
+    $('.modal__close').on('click', function() {
+        $('#modal').fadeOut();
+        $('.modal__mask').fadeOut();
+    });
+});
--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-    
 
     $('#submit-btn').on('click', function(){
         let fText = '';
@@ -9,12 +8,14 @@ $(document).ready(function() {
         let tHashtag = '';
         let encodedParam = '';
         let encodedArr = new Array();
+        $('.head-message').removeClass('head-message-copy');
 
         fText = $('[name=f-text]').val();
         fUrl = $('#f-url').val();
         tAccount1 = $('#t-account1').val();
         tAccount2 = $('#t-account2').val();
         tHashtag = $('#t-hashtag').val();
+        tCard = $('[name=card]').val();
 
         let valArr = new Array(fText, fUrl, tAccount1, tAccount2, tHashtag);
         for(let i = 0; i < valArr.length; i++){
@@ -29,29 +30,10 @@ $(document).ready(function() {
         let enTHashtag = encodedArr[4];
         
         // htmlの出力
-        $('.tags__html-title').text('HTML');
-        $('.tags__html-code-url').text('<meta property="og:url" content="' + fUrl + '" />');
-        $('.tags__html-code-title').text('<meta property="og:title" content="ページのタイトル" />');
-        $('.tags__html-code-type').text('<meta property="og:type" content="ページのタイプ">');
-        $('.tags__html-code-description').text('<meta property="og:description" content="' + fText + '" />');
-        $('.tags__html-code-image').text('<meta property="og:image" content="画像のURL" />');
-        $('.tags__html-code-site_name').text('<meta property="og:site_name" content="サイト名" />');
-        $('.tags__html-code-local').text('<meta property="og:locale" content="ja_JP" />');
-        $('.tags__html-code-app_id').text('<meta property="fb:app_id" content="appIDを入力" />');
-        $('.tags__html-code-twitter-card').text('<meta name="twitter:card" content="カード種類" />');
-        $('.tags__html-code-twitter-site').text('<meta name="twitter:site" content="@ユーザー名" />');
-        //pug
-        $('.tags__pug-title').text('Pug');
-        $('.tags__pug-code-url').text('meta(property="og:url", content="' + fUrl + '")');
-        $('.tags__pug-code-title').text('meta(property="og:title", content="ページのタイトル")');
-        $('.tags__pug-code-type').text('meta(property="og:type", content="ページのタイプ")');
-        $('.tags__pug-code-description').text('meta(property="og:description", content="' + fText + '")');
-        $('.tags__pug-code-image').text('meta(property="og:image", content="画像のURL")');
-        $('.tags__pug-code-site_name').text('meta(property="og:site_name", content="サイト名")');
-        $('.tags__pug-code-local').text('meta(property="og:locale", content="ja_JP")');
-        $('.tags__pug-code-app_id').text('meta(property="fb:app_id", content="appIDを入力")');
-        $('.tags__pug-code-twitter-card').text('meta(name="twitter:card", content="カード種類")');
-        $('.tags__pug-code-twitter-site').text('meta(name="twitter:site", content="@ユーザー名")');
+        $('.tags__include-url--js').text(fUrl);
+        $('.tags__html-code-twitter-card-val---js').text(tCard);
+
+
         //url
         $('.tags__url-title').text('シェアリンクのURL');
         
@@ -64,5 +46,95 @@ $(document).ready(function() {
         $('.tags__facebook-title').text('Facebook');
         $('.tags__facebook-url-code').text('https://www.facebook.com/sharer/sharer.php?u='+ enFUrl + '');
         
+        
+        $('.head-message').addClass('head-message-orange');
+        $('.head-message__text').text('');
+        $('.head-message__text').text('Completed!');
+        $('#modal').fadeOut();
+        $('.modal__mask').fadeOut();
+        setTimeout(function() {
+            $('.head-message').slideDown();
+        }, 200);
+        setTimeout(function() {
+            $('.head-message').fadeOut();
+        }, 5000);
+
+    });
+
+    // コードをまとめてコピー
+    $('.copy-btn').on('click', function(){
+        $('.head-message').removeClass('head-message-orange');
+        let target = $(this).siblings().children('.copy-area').text();
+        let textarea = $('<textarea></textarea>');
+        textarea.text(target);
+        $(this).append(textarea);
+        textarea.select();
+        document.execCommand('copy');
+        textarea.remove();
+        
+        $('.head-message').addClass('head-message-copy');
+        $('.head-message__text').text('');
+        $('.head-message__text').text('Copied the Code!');
+        $('#modal').fadeOut();
+        $('.modal__mask').fadeOut();
+        setTimeout(function() {
+            $('.head-message').slideDown();
+        }, 200);
+        setTimeout(function() {
+            $('.head-message').fadeOut();
+        }, 5000);
+    });
+    // コードを一行ずつコピー
+    $('.fa-arrow-circle-right').on('click', function () {
+        $('.head-message').removeClass('head-message-orange');
+
+        let distParent = $(this).parent();
+        let clickIndex = distParent.children('.fas').index(this) + 1;
+        let lineTarget = distParent.siblings().children().children('.copy-area p:nth-child('+ clickIndex + ')').text();
+        let textarea = $('<textarea></textarea>');
+        textarea.text(lineTarget);
+        $(this).append(textarea);
+        textarea.select();
+        document.execCommand('copy');
+        textarea.remove();
+
+        $('.head-message').addClass('head-message-copy');
+        $('.head-message__text').text('');
+        $('.head-message__text').text('Copied the Code!');
+        $('#modal').fadeOut();
+        $('.modal__mask').fadeOut();
+        setTimeout(function() {
+            $('.head-message').slideDown();
+        }, 200);
+        setTimeout(function() {
+            $('.head-message').fadeOut();
+        }, 5000);
     });
 });
+
+
+// // htmlの出力
+        // $('.tags__html-title').text('HTML');
+        // $('.tags__html-code-url').text('<meta property="og:url" content="' + fUrl + '" />');
+        // $('.tags__html-code-title').text('<meta property="og:title" content="ページのタイトル" />');
+        // $('.tags__html-code-type').text('<meta property="og:type" content="ページのタイプ">');
+        // $('.tags__html-code-description').text('<meta property="og:description" content="' + fText + '" />');
+        // $('.tags__html-code-image').text(s'<meta property="og:image" content="画像のURL" />');
+        // $('.tags__html-code-site_name').text('<meta property="og:site_name" content="サイト名" />');
+        // $('.tags__html-code-local').text('<meta property="og:locale" content="ja_JP" />');
+        // $('.tags__html-code-app_id').text('<meta property="fb:app_id" content="appIDを入力" />');
+        // $('.tags__html-code-twitter-card').text('<meta name="twitter:card" content="カード種類" />');
+        // $('.tags__html-code-twitter-site').text('<meta name="twitter:site" content="@ユーザー名" />');
+        // //pug
+        // $('.tags__pug-title').text('Pug');
+        // $('.tags__pug-code-url').text('meta(property="og:url", content="' + fUrl + '")');
+        // $('.tags__pug-code-title').text('meta(property="og:title", content="ページのタイトル")');
+        // $('.tags__pug-code-type').text('meta(property="og:type", content="ページのタイプ")');
+        // $('.tags__pug-code-description').text('meta(property="og:description", content="' + fText + '")');
+        // $('.tags__pug-code-image').text('meta(property="og:image", content="画像のURL")');
+        // $('.tags__pug-code-site_name').text('meta(property="og:site_name", content="サイト名")');
+        // $('.tags__pug-code-local').text('meta(property="og:locale", content="ja_JP")');
+        // $('.tags__pug-code-app_id').text('meta(property="fb:app_id", content="appIDを入力")');
+        // $('.tags__pug-code-twitter-card').text('meta(name="twitter:card", content="カード種類")');
+        // $('.tags__pug-code-twitter-site').text('meta(name="twitter:site", content="@ユーザー名")');
+        //url
